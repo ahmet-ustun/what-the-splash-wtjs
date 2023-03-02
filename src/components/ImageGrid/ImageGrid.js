2ac5c83ea7a765810e6132688f8bbd00716e2b1d
Ahmet Ustun <austu@softserveinc.com>
2023-03-02 10:24:05 +0200

    Use Lightbox in order to spotlight the images in screens wider than 1024px

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Lightbox from 'react-image-lightbox';
 
 import { loadImagesData } from '../../store/actions';
 import { Button } from '../Button';
@@ -8,12 +9,28 @@ import { Stats } from '../Stats';
 import './styles.css';
 
 class ImageGrid extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLightboxOpened: false,
+      selectedImageUrl: '',
+      selectedImageAlt: '',
+    };
+  }
+
   componentDidMount() {
     this.props.loadImages();
   }
 
   render() {
     const { isLoading, images, error, loadImages, stats } = this.props;
+    const { isLightboxOpened, selectedImageUrl, selectedImageAlt } = this.state;
+
+    const resolution = window.innerWidth;
+    const isMobile = resolution >= 320 && resolution <= 480;
+    const isTablet = resolution >= 768 && resolution <= 1024;
+    const isDesktop = !isMobile && !isTablet;
 
     return (
       <div className="content">
@@ -22,12 +39,33 @@ class ImageGrid extends Component {
             <div
               key={image.id}
               className={`item item-${Math.ceil(image.height / image.width)}`}
+              onClick={() =>
+                this.setState({
+                  isLightboxOpened: true,
+                  selectedImageUrl: image.urls.regular,
+                  selectedImageAlt: image['alt_description'],
+                })
+              }
             >
               <Stats stats={stats[image.id]} />
               <img src={image.urls.small} alt={image.user.username} />
             </div>
           ))}
         </section>
+        {isDesktop && isLightboxOpened && (
+          <Lightbox
+            mainSrc={selectedImageUrl}
+            imageTitle={selectedImageAlt}
+            enableZoom={false}
+            onCloseRequest={() =>
+              this.setState({
+                isLightboxOpened: false,
+                selectedImageUrl: '',
+                selectedImageAlt: '',
+              })
+            }
+          />
+        )}
         {error && <div className="error">{JSON.stringify(error)}</div>}
         <Button
           onClick={() => !isLoading && loadImages()}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import 'react-image-lightbox/style.css';
 import App from './components/App.js';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
