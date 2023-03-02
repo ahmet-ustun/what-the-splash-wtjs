22b2b8f38995e7f41162574b729bf05bb5b877e3
Ahmet Ustun <austu@softserveinc.com>
2023-03-02 13:07:06 +0200

    Remove the emojis as they needed some configuration

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -4,14 +4,14 @@ import './styles.css';
 
 export const Stats = ({ stats }) => {
   if (!stats) {
-    return <span className="stats">🙄 Loading...</span>;
+    return <span className="stats">Loading...</span>;
   }
 
   return (
     <span className="stats">
-      {stats.isLoading && '🙄 Loading...'}
-      {stats.stats && `🤘 ${stats.stats}`}
-      {stats.error && '🤯 Error!'}
+      {stats.isLoading && 'Loading...'}
+      {stats.stats && `${stats.stats}`}
+      {stats.error && 'Error!'}
     </span>
   );
 };
